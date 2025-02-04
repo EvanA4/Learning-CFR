ae94195cd2a0f33e81af78c151110e4c4740458f
Evan Abbott <107959883+EvanA4@users.noreply.github.com>
2025-02-04 18:28:19 -0500

    I DID IITTTTTTTTT WOOOOOOOOOOOOOOOOHOOOOOO

--- kuhn.py
+++ kuhn.py
@@ -1,7 +1,8 @@
 from tabulate import tabulate
+import matplotlib.pyplot as plt
 
 # Define global constants
-ITERATIONS = 1
+ITERATIONS = 300000
 
 CARDS = [
     "K", "Q", "J"
@@ -12,16 +13,39 @@ ACTIONS = [
 ]
 
 TERMINAL_ACTION_SET = {
-    "pp", "bb", "pbp", "pbb"
+    "pp", "bb", "pbp", "pbb", "bp"
 }
 
 INFOSET_STRS = [
-    "K", "Q", "J",
-    "Kb", "Kp",
-    "Qb", "Qp",
-    "Jb", "Jp",
-    "Kpb", "Qpb", "Jpb"
+    "K",
+    "Q", 
+    "J",
+    "Kp",
+    "Qp",
+    "Jp",
+    "Kb",
+    "Qb",
+    "Jb",
+    "Kpb",
+    "Qpb",
+    "Jpb"
 ]
+
+# Define DEBUG constants
+BRYCE_STRATS = {
+    "K": [2/3, 1/3],
+    "Q": [1/2, 1/2],
+    "J": [1/3, 2/3],
+    "Kp": [1, 0],
+    "Qp": [2/3, 1/3],
+    "Jp": [1/3, 2/3],
+    "Kb": [1, 0],
+    "Qb": [1/2, 1/2],
+    "Jb": [0, 1],
+    "Kpb": [1, 0],
+    "Qpb": [1/2, 1/2],
+    "Jpb": [0, 1],
+}
 
 # Define infoSets map
 infoSets: dict[str, type['InfoSet']] = {}
@@ -30,13 +54,14 @@ cumulativeGains = 0
 
 # Helper functions
 def getGameUtility(pocket1: str, pocket2: str, isBet: bool):
-    return (CARDS.index(pocket1) < CARDS.index(pocket2)) * (1 + isBet)
+    isWin = CARDS.index(pocket1) < CARDS.index(pocket2)
+    return (1 if isWin else -1) * (1 + isBet)
 
 
 class InfoSet():
     def __init__(self, setStr: str) -> None:
         self.setStr = setStr
-        self.gainsSum = [0, 0]
+        self.gainsSum = [.5, .5]
         self.strategy = [0, 0]
         self.beliefs = [0, 0]
         self.utils = [0, 0]
@@ -63,6 +88,9 @@ class InfoSet():
         else:
             self.strategy[0] = 1/2
             self.strategy[1] = 1/2
+
+    def debugSetStrategies(self) -> None:
+        self.strategy = BRYCE_STRATS[self.setStr]
     
     def setBeliefs(self) -> None:
         # split computation based on depth of node
@@ -88,7 +116,7 @@ class InfoSet():
     def setUtils(self) -> None:
         for ACTION in ACTIONS:
             # check if terminating
-            actionStr = self.setStr[1:-1] + ACTION
+            actionStr = self.setStr[1:] + ACTION
             if actionStr in TERMINAL_ACTION_SET:
                 parents = self.getParents()
 
@@ -116,7 +144,7 @@ class InfoSet():
                         self.utils[0] += self.beliefs[ctr] * (child.strategy[0] * getGameUtility(self.setStr[0], CARD, True))
                         # if player 1 passes
                         self.utils[0] += self.beliefs[ctr] * (child.strategy[1] * 1)
-                        ctr += 0
+                        ctr += 1
 
             # calculate utilities for the root node's corresponding action :(
             else:
@@ -144,14 +172,14 @@ class InfoSet():
                                 child.strategy[1] * getGameUtility(self.setStr[0], CARD, False)
                             )
                             # if player 2 plays bet
-                            grandchild = infoSets[CARD + "pb"]
+                            grandchild = infoSets[self.setStr[0] + "pb"]
                             self.utils[1] += .5 * (
                                 child.strategy[0] * grandchild.utility
                             )
                 
         self.utility = self.strategy[0] * self.utils[0] + self.strategy[1] * self.utils[1]
 
-    def setReachProbability(self):
+    def setReachProbability(self) -> None:
         # if it's a root set, then it's just 1/3 
         if len(self.setStr) == 1:
             self.reach = 1/3
@@ -172,10 +200,10 @@ class InfoSet():
                 parent = infoSets[parentStr]
                 self.reach += 1/3 * 1/2 * parent.strategy[0]
 
-    def setGains(self):
+    def setGains(self) -> None:
         # if gain is < 0, set it to 0
-        betGain = (self.utils[0] - self.utility) if (self.utils[0] - self.utility) * self.reach > 0 else 0
-        passGain = (self.utils[1] - self.utility) if (self.utils[1] - self.utility) * self.reach > 0 else 0
+        betGain = (self.utils[0] - self.utility) * self.reach if (self.utils[0] - self.utility) > 0 else 0
+        passGain = (self.utils[1] - self.utility) * self.reach if (self.utils[1] - self.utility) > 0 else 0
 
         self.gainsSum[0] += betGain
         self.gainsSum[1] += passGain
@@ -184,6 +212,8 @@ class InfoSet():
 
 
 def debugPrintSets():
+    global infoSets
+
     headers = [
         "InfoSet","Strat:Bet", "Strat:Pass",
         "---","Belief:H", "Belief:L", "---",
@@ -194,35 +224,35 @@ def debugPrintSets():
     for infoSetStr in INFOSET_STRS:
         infoSet = infoSets[infoSetStr]
         row=[
-            infoSetStr, *infoSet.strategy,
-            infoSetStr, *infoSet.beliefs, infoSetStr,
-            *infoSet.utils, infoSet.utility,
-            infoSet.reach
+            infoSetStr, f'{infoSet.strategy[0]:.2f}', f'{infoSet.strategy[1]:.2f}',
+            infoSetStr, f'{infoSet.beliefs[0]:.2f}', f'{infoSet.beliefs[1]:.2f}', infoSetStr,
+            f'{infoSet.utils[0]:.2f}', f'{infoSet.utils[1]:.2f}', f'{infoSet.utility:.2f}',
+            f'{infoSet.reach:.2f}'
         ]
         rows.append(row)
     print(tabulate(rows, headers=headers,tablefmt="pretty",stralign="left"))
 
 
-def cfr():
+def cfr(ctr: int):
     global infoSets
     # strategies
     for infoSetStr in INFOSET_STRS:
         infoSets[infoSetStr].setStrategies()
+        if (ctr == 0):
+            infoSets[infoSetStr].debugSetStrategies()
 
     # beliefs
     for infoSetStr in INFOSET_STRS:
         infoSets[infoSetStr].setBeliefs()
-        # print(f"{infoSetStr}: {infoSets[infoSetStr].beliefs}")
 
     # utilities of each action, but go BACKWARDS
     for i in range(len(INFOSET_STRS) - 1, -1, -1):
         infoSets[INFOSET_STRS[i]].setUtils()
-        # print(f"{INFOSET_STRS[i]}: {infoSets[INFOSET_STRS[i]].utils}")
         # also do reach probabilities while we're at it
         infoSets[INFOSET_STRS[i]].setReachProbability()
 
     # DEBUG PRINTING
-    debugPrintSets()
+    # debugPrintSets()
 
     # update gains
     global cumulativeGains
@@ -249,10 +279,10 @@ def printResults():
     for infoSetStr in INFOSET_STRS:
         infoSet = infoSets[infoSetStr]
         row=[
-            infoSetStr, *infoSet.strategy,
-            infoSetStr, *infoSet.beliefs, infoSetStr,
-            *infoSet.utils, infoSet.utility,
-            infoSet.reach, infoSetStr, *infoSet.gainsSum
+            infoSetStr, f'{infoSet.strategy[0]:.2f}', f'{infoSet.strategy[1]:.2f}',
+            infoSetStr, f'{infoSet.beliefs[0]:.2f}', f'{infoSet.beliefs[1]:.2f}', infoSetStr,
+            f'{infoSet.utils[0]:.2f}', f'{infoSet.utils[1]:.2f}', f'{infoSet.utility:.2f}',
+            f'{infoSet.reach:.2f}', infoSetStr, f'{infoSet.gainsSum[0]:.2f}', f'{infoSet.gainsSum[1]:.2f}'
         ]
         rows.append(row)
     print(tabulate(rows, headers=headers,tablefmt="pretty",stralign="left"))
@@ -262,13 +292,53 @@ def main():
     # initialize info sets
     initSets()
 
+    # initialize matplotlib graph
+    gains = []
+    iterations = []
+
     # begin actual computation
+    NUM_PRINTS = 50
+    tenth = ITERATIONS // NUM_PRINTS
     for i in range(ITERATIONS):
-        cfr()
-        print(cumulativeGains)
+        cfr(i)
+        if i % tenth == 0:
+            print(f"{i // tenth * (100 // NUM_PRINTS) : 2.0f}%: {cumulativeGains}")
+            gains.append(cumulativeGains)
+            iterations.append(i)
 
     # print fine-tuned strategy
-    # printResults()
+    print()
+    printResults()
+
+    # display matplotlib graph
+    plt.figure(figsize=(10, 6))
+    plt.plot(iterations, gains)
+    plt.title("Gains vs. Iterations")
+    plt.xlabel("Iterations")
+    plt.ylabel("Gains")
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
+
+
+'''
+EXPECTED RESULTS:
++---------+-----------+------------+
+| InfoSet | Strat:Bet | Strat:Pass |
++---------+-----------+------------+
+| K       | 0.9       | 0.11       |
+| Q       | 0.02      | 0.98       |
+| J       | 0.3       | 0.7        |
+| Kb      | 1.0       | 0.0        |
+| Kp      | 0.97      | 0.03       |
+| Qb      | 0.32      | 0.70       |
+| Qp      | 0.01      | 0.99       |
+| Jb      | 0.0       | 1.0        |
+| Jp      | 0.3       | 0.6        |
+| Kpb     | 1.0       | 0.0        |
+| Qpb     | 0.6       | 0.3        |
+| Jpb     | 0.0       | 1.0        |
++---------+-----------+------------+
+'''
